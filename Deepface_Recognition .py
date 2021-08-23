027a0ca1d15bf0e992da12498dcf36e81694bc8b
Dora-H <70878758+Dora-H@users.noreply.github.com>
2021-08-23 23:05:34 +0800

    Update Deepface_Recognition .py

--- Deepface_Recognition .py
+++ Deepface_Recognition .py
@@ -1,76 +1,57 @@
-# import OpenCV module
 import cv2
-# import deepface module
 from deepface import DeepFace
 
 
 class FaceRecognition(object):
     def __init__(self):
-        # define paths
-        self.pathA = 'C:\\Users\\utaipei\\PycharmProjects\\pythonProject00000\\day07\\haar\\eye.xml'
-        self.pathB = 'C:\\Users\\utaipei\\PycharmProjects\\pythonProject00000\\day07\\haar\\face.xml'
-        self.pathC = 'C:\\Users\\utaipei\\PycharmProjects\\pythonProject00000\\day07\\haar\\nose.xml'
-        self.pathF1 = 'C:\\Users\\utaipei\\PycharmProjects\\pythonProject00000\\day07\\Kim.jpg'
-        self.pathF2 = 'C:\\Users\\utaipei\\PycharmProjects\\pythonProject00000\\day07\\Nam.jpg'
+        self.pathA = 'your own path\\haar\\eye.xml'
+        self.pathB = 'your own path\\haar\\face.xml'
+        self.pathC = 'your own path\\haar\\nose.xml'
+        self.pathF1 = 'your own path\\day07\\Kim.jpg'
+        self.pathF2 = 'your own path\\day07\\Nam.jpg'
 
         result = DeepFace.verify(self.pathF1, self.pathF2)
         print("Is verified: ", result["verified"])
 
     def classifiers(self):
-        # load OpenCV face detector
         eye_cascade = cv2.CascadeClassifier(self.pathA)
         face_cascade = cv2.CascadeClassifier(self.pathB)
         nose_cascade = cv2.CascadeClassifier(self.pathC)
 
-        # list to hold all subject faces
         faces_holder = []
-
-        # set images in a list & verify
         datasets = [self.pathF1, self.pathF2]
 
-        # read image
         for image in datasets:
             image = cv2.imread(image)
 
             faces = face_cascade.detectMultiScale(image, 1.08, 5, minSize=(50, 50))
             for left, top, width, height in faces:
-                # extract the faces area
                 faces = image[top:top+height, left:left+width]
-                cv2.rectangle(image, (left, top), (left+width, top+height),
-                              (13, 230, 23), 2)
+                cv2.rectangle(image, (left, top), (left+width, top+height), (13, 230, 23), 2)
 
-                # detect noses area from faces area
                 noses = nose_cascade.detectMultiScale(faces, 1.15, 5,
                                                       flags=cv2.CASCADE_SCALE_IMAGE)
                 for (nx, ny, nw, nh) in noses:
                     cv2.rectangle(faces, (nx, ny), (nx+nw, ny+nh), (255, 0, 255), 2)
 
-                # detect eyes area from faces area
-                eyes = eye_cascade.detectMultiScale(faces, 1.100, 4,
-                                                    flags=cv2.CASCADE_SCALE_IMAGE)
+                eyes = eye_cascade.detectMultiScale(faces, 1.100, 4, flags=cv2.CASCADE_SCALE_IMAGE)
                 for(ex, ey, ew, eh) in eyes:
                     cv2.rectangle(faces, (ex, ey), (ex+ew, ew+eh), (255, 255, 255), 2)
 
                 if faces is not None:
-                    # add face to list of faces_holder
                     faces_holder.append(image[top:top+height, left:left+width])
 
-            # to analyze age, gender, emotion
             demography = DeepFace.analyze(image, ['emotion', 'age', 'gender'])
-
-            # print total faces (text color:white)
             input_strings = "Age:{} Gender:{} Emotion:{} "
             cv2.putText(image, input_strings.format(str(demography["age"]),
                                                     demography["gender"],
                                                     demography["dominant_emotion"]),
                         (150, 300), cv2.FONT_HERSHEY_PLAIN, 2, (13, 230, 23), 2)
 
-            # display an image window to show the image
             cv2.imshow('DeepFace Recognition ', image)
             cv2.waitKey()
             cv2.destroyAllWindows()
 
-        # print the quantity of detected faces.
         print('Total detected faces:%d' % len(faces_holder))
 
 
